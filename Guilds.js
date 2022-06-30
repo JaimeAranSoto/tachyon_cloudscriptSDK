d27fa73dd01af459404219320747b6cdded5af93
Jaime Arancibia Soto <jaimearansoto@gmail.com>
2022-06-30 12:21:20 -0400

    Create Guilds.js

---
+++ Guilds.js
@@ -0,0 +1,66 @@
+handlers.VoteForGuildWar = function (args) {
+    // -2 = Player has already voted (allow to change opinion??)
+    // -1 = Guild (mine or targeted) doesn't exist
+
+    var approve = args.approve; //bool
+    var targetedGuildId = args.targetedGuildId;
+
+    var myGuild = entity.ListMembership[0];
+
+    if (myGuild == null || myGuild === undefined) {
+        log.debug("Current player is not in a guild");
+        return -1;
+    }
+
+    var myGuildObjects = entity.GetObjects({ Entity: myGuild.Group });
+
+    if (myGuildObjects == null || myGuildObjects === undefined) {
+        log.debug("PlayerGuild has no objects");
+        return -1;
+    }
+
+    var myGuildObjectResult = myGuild.Objects;
+    var votings = myGuildObjectResult.Votings;
+
+    log.debug("Previous Votings", votings);
+
+    var approveVotes = [];
+    var denyVotes = [];
+    if (approve) {
+        approveVotes.push(currentPlayerId);
+    } else {
+        denyVotes.push(currentPlayerId);
+    }
+
+    votings.forEach(voting => {
+        if (voting.enemyGuildId == targetedGuildId) {
+
+            voting.approveVotes.forEach(vote => {
+                if (vote == currentPlayerId) {
+                    return -2;
+                }
+            });
+            voting.denyVotes.forEach(vote => {
+                if (vote == currentPlayerId) {
+                    return -2;
+                }
+            });
+
+            approveVotes.concat(voting.approveVotes);
+            denyVotes.concat(voting.denyVotes);
+        }
+    });
+
+    var newVoting = { approveVotes: approveVotes, denyVotes: denyVotes, enemyGuildId: targetedGuildId };
+    log.debug("New Singular Voting", newVoting);
+
+    for (let i = 0; i < votings.length; i++) {
+        if (votings[i].enemyGuildId == targetedGuildId) {
+            votings[i] = newVoting; //Replace old voting
+        }
+    }
+    log.debug("New Votings", votings);
+
+    entity.SetObjects({ Entity: myGuild.Group, Objects: myGuildObjectResult });
+    return 1;
+}
