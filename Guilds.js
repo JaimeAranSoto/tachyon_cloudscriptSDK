904980f676a905b17cf0581d94bfc63af9b4e46f
Jaime Arancibia Soto <jaimearansoto@gmail.com>
2022-09-27 15:15:34 -0400

    Update Guilds.js

--- Guilds.js
+++ Guilds.js
@@ -1,88 +1,3 @@
-/*handlers.VoteForGuildWar = function (args, context) {
-    // -2 = Player has already voted (allow to change opinion?? To be discussed...)
-    // -1 = Guild (mine or targeted) doesn't exist
-
-    var myEntityId = args.myEntityId;
-    var approve = args.approve; //bool
-    var targetedGuildId = args.targetedGuildId;
-    var allMyGuilds = entity.ListMembership({ Entity: { Id: myEntityId, Type: "title_player_account" } });
-    var myGuild = allMyGuilds.Groups[0];
-
-    if (myGuild == null || myGuild === undefined) {
-        log.debug("Current player is not in a guild", allMyGuilds);
-        return -1;
-    } else {
-        log.debug("Guild found", myGuild);
-    }
-
-    var getObjectsResult = entity.GetObjects({ Entity: myGuild.Group });
-
-    if (getObjectsResult == null || getObjectsResult === undefined) {
-        log.debug("PlayerGuild has no objects");
-        return -1;
-    }
-
-    var myGuildObjects = getObjectsResult.Objects;
-
-    if (myGuildObjects.Votings === undefined) {
-        myGuildObjects.Votings = { ObjectName: "Votings", DataObject: {} };
-    }
-    var votings = myGuildObjects.Votings.DataObject;
-
-    var approveVotes = [];
-    var denyVotes = [];
-    if (approve) {
-        approveVotes.push(myEntityId);
-    } else {
-        denyVotes.push(myEntityId);
-    }
-
-    for (let i = 0; i < votings.length; i++) {
-        var voting = votings[i];
-        if (voting.enemyGuild == targetedGuildId) {
-            for (let j = 0; j < voting.approveVotes.length; j++) {
-                var vote = voting.approveVotes[j];
-                approveVotes.push(vote);
-                if (vote == myEntityId) {
-                    return -2;
-                }
-            }
-            for (let j = 0; j < voting.denyVotes.length; j++) {
-                var vote = voting.denyVotes[j];
-                denyVotes.push(vote);
-                if (vote == myEntityId) {
-                    return -2;
-                }
-            }
-        }
-    }
-
-    var newVoting = { approveVotes: approveVotes, denyVotes: denyVotes, enemyGuild: targetedGuildId };
-    log.debug("New Singular Voting", newVoting);
-
-    if (votings == null || votings === undefined || votings.length == 0) {
-        votings = [];
-        votings[0] = newVoting;
-    } else {
-        for (let i = 0; i < votings.length; i++) {
-            if (votings[i].enemyGuild == targetedGuildId) {
-                votings[i] = newVoting; //Replace old voting
-                log.debug("Previous voting replaced");
-                break;
-            }
-            if (i == votings.length - 1) {
-                votings.push(newVoting);
-                break;
-            }
-        }
-    }
-    log.debug("New Votings", votings);
-    entity.SetObjects({ Entity: { Id: myGuild.Group.Id, Type: "group" }, Objects: [{ ObjectName: "Votings", DataObject: votings }] });
-    // entity.SetObjects({ Entity: { Id: myGuild.Group.Id, Type: "group" }, Objects: myGuildObjects });
-    return 1;
-}*/
-
-
 handlers.CheckExpirationForBattleInvitation = function (args) {
     const EXPIRATION_TIME = 3 * 60; //Seconds
     const BATTLE_MAX_DURATION = 5 * 60;
@@ -141,6 +56,7 @@ handlers.AcceptOrCreateBattleInvitation = function (args) {
     var targetedGuildId = args.targetedGuildId; //this could be null
     var date = args.date;
 
+    var myGuild = GetMyGuild(myEntityId);
     var myGuildObjects = GetMyGuildObjects(myEntityId);
 
     var isNewInvitation = false;
@@ -239,59 +155,6 @@ GetMyGuild = function (playerId) {
     }
     return myGuild;
 }
-
-/*
-handlers.VoteForGuildWar = function (args, context) {
-    var myEntityId = args.myEntityId;
-    var vote = args.vote; //bool
-    var targetedGuildId = args.targetedGuildId; //this could be null
-    var date = args.date;
- 
-    var allMyGuilds = entity.ListMembership({ Entity: { Id: myEntityId, Type: "title_player_account" } });
-    var myGuild = allMyGuilds.Groups[0];
- 
-    if (myGuild == null || myGuild === undefined) {
-        log.debug("Current player is not in a guild", allMyGuilds);
-        return -1;
-    } else {
-        log.debug("Guild found", myGuild);
-    }
- 
-    var getObjectsResult = entity.GetObjects({ Entity: myGuild.Group });
- 
-    if (getObjectsResult == null || getObjectsResult === undefined) {
-        log.debug("PlayerGuild has no objects");
-        return -1;
-    }
- 
-    var myGuildObjects = getObjectsResult.Objects;
- 
-    var isNewAttack = false;
- 
-    if (myGuildObjects.guildAttack === undefined) {
-        myGuildObjects.guildAttack = { ObjectName: "guildAttack", DataObject: {} };
-        isNewAttack = true;
-    }
-    var votings = myGuildObjects.guildAttack.DataObject;
-    votings.guildId = targetedGuildId;
-    //if (isNewAttack) { //This will be reactivated again
-    votings.responsible = myEntityId;
-    votings.date = date;
-    //}
- 
-    if (votings.yes == null) votings.yes = [];
-    if (votings.no == null) votings.no = [];
- 
-    if (vote && !votings.yes.includes(myEntityId)) {
-        votings.yes.push(myEntityId);
-    } else if (!votings.no.includes(myEntityId)) {
-        votings.no.push(myEntityId);
-    }
- 
-    entity.SetObjects({ Entity: { Id: myGuild.Group.Id, Type: "group" }, Objects: [{ ObjectName: "guildAttack", DataObject: votings }] });
-    return 1;
-}
-*/
 
 handlers.GetGuildObjects = function (args) {
     var guildId = args.guildId;
