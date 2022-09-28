e4c063d3c7f855e2b0aa9b96a1d1898f2c6d9521
Jaime Arancibia Soto <jaimearansoto@gmail.com>
2022-09-28 12:26:06 -0400

    Update Guilds.js

--- Guilds.js
+++ Guilds.js
@@ -118,8 +118,8 @@ handlers.FinishWar = function (args) {
     if (myGuildObjects.battleInvitation != null) {
         var invitation = myGuildObjects.battleInvitation.DataObject;
         if (invitation.participants.includes(myEntityId) || invitation.leader == myEntityId) {
-            SplitWarPoints(attackerGuild, didAttackersWon, true);
-            SplitWarPoints(defenderGuild, !didAttackersWon, false);
+            SplitWarPoints(attackerGuild, didAttackersWon, false);
+            SplitWarPoints(defenderGuild, !didAttackersWon, true);
             return;
             var newInvitation = { leader: "", participants: [], successful: false };
             entity.SetObjects({ Entity: { Id: attackerGuild, Type: "group" }, Objects: [{ ObjectName: "battleInvitation", DataObject: newInvitation }] });
