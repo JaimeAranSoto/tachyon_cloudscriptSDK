3b4a91462625ac6d4c5d1cbb2be8e83c503ca0d9
Jaime Arancibia Soto <jaimearansoto@gmail.com>
2022-10-13 11:41:17 -0300

    Update Players.js

--- Players.js
+++ Players.js
@@ -37,7 +37,7 @@ handlers.GetDisplayNames = function (args) {
         entities.push({ Id: ids, Type: "title_player_account" });
         const playerProfile = ids[i];
     }
-    var playerProfiles = server.GetProfiles({ Entities: entities });
+    var playerProfiles = entity.GetProfiles({ Entities: entities });
 
     for (let i = 0; i < playerProfiles.length; i++) {
         const profile = playerProfiles[i];
