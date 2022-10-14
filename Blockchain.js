e467ea8da274bbc87552aa58b233bcea279c29e4
Jaime Arancibia Soto <jaimearansoto@gmail.com>
2022-10-14 13:20:32 -0300

    Update Blockchain.js

--- Blockchain.js
+++ Blockchain.js
@@ -89,7 +89,11 @@ handlers.ConfirmPurchase = function (args) {
         const storedPurchase = purchasesData[i];
         if (storedPurchase.purchaseId == purchaseId) {
             //--Buy item or do whatever is needed to do--//
-            var data = JSON.parse(internalData.confirmedPurchases.Value);
+            if (internalData.confirmedPurchases == null) {
+                var data = [];
+            } else {
+                var data = JSON.parse(internalData.confirmedPurchases.Value);
+            }
             data.push(storedPurchase);
             server.UpdateUserInternalData({
                 PlayFabId: currentPlayerId, Data: {
